e61a2aab88f0d88648d4a65b9d5f392a9c8b8f52
Ramon Velasco <ramonmaito@gmail.com>
2020-08-21 22:14:52 -0300

    v51

---
+++ src/about/about.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name ='Sobre' small='Nós'></PageHeader>
+        <h2>Nossa Historia</h2>
+        <p>ASDASD ASDjas ADASiojsaD</p>
+        <h2>Missão e Visão</h2>
+        <p>ASDjoisad asdijoasd asoid</p>
+        <h2>Imprensa</h2>
+        <p>asdjoi asjioad asoijas asjoi</p>
+    </div>
+)
